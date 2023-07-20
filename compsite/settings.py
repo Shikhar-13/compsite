be1bb7beacf5dc2ad57bcb72883bd2b5cbb9bc13
Shikhar-13 <shikharguptaoct13@gmail.com>
2023-07-20 13:58:57 +0530

    static

--- compsite/settings.py
+++ compsite/settings.py
@@ -126,7 +126,7 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 STATICFILES_DIRS = [
-    BASE_DIR / 'compsite_pro/static',
+    BASE_DIR / 'static',
     "users/static",
 ]
 # Default primary key field type

45b341b9338e1e99215c380cc64952d7bc4a0d93
Shikhar-13 <shikharguptaoct13@gmail.com>
2023-07-30 17:43:59 +0530

    email fix

--- users/forms.py
+++ users/forms.py
@@ -1,14 +1,21 @@
 from django.contrib.auth.forms import UserCreationForm
 from django.contrib.auth.forms import User
 from django import forms
+from django.core.exceptions import ValidationError
 
 class RegisterUserForm(UserCreationForm):
     first_name=forms.CharField()
     last_name=forms.CharField()
-    Email=forms.EmailField()
-    Branch= forms.ChoiceField(choices=[('Computer Engineering','Computer engineering')])
-    Select_Year = forms.ChoiceField(choices=[ ('second year','Second Year')])
+    email=forms.EmailField()
+    Branch= forms.ChoiceField(choices=[('',''),('Computer Engineering','Computer engineering'),('IT','IT')])
+    Select_Year = forms.ChoiceField(choices=[('', ''), ('first year', 'First Year'), ('second year','Second Year')])
 
     class Meta:
         model = User
-        fields=('username','first_name','last_name','Email','Select_Year','Branch','password1','password2')
+        fields=('username','first_name','last_name','email','Select_Year','Branch','password1','password2')
+    
+    def clean(self):
+       email = self.cleaned_data.get('email')
+       if User.objects.filter(email=email).exists():
+            raise ValidationError("Email exists")
+       return self.cleaned_data
